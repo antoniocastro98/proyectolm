c53211d9008c7e5e0ee6c196f21ada65292ed2e1
root <antoniocastro-@hotmail.com>
2021-03-05 12:08:33 +0100

    Añadidos ficheros de funciones y el ejercicio

---
+++ Ejercicio.py
@@ -0,0 +1,74 @@
+from lxml import etree
+from Funciones import listar, contar, buscar, informacion
+datos=etree.parse("archivoxml.xml")
+
+
+print(''' MENU
+1.Listar información: Mostrar el nombre de las canales de los que tenemos información.
+
+2.Contar información: Mostrar la cantidad de canales que son de deportes.
+
+3.Buscar o filtrar información: Pedir por teclado el nombre de un canal y mostramos la programación.
+
+4.Buscar información relacionada: introducir el nombre de un programa y mostrar el canal donde se emite.
+
+5.Ejercicio libre: Introducir un idioma y mostrar los canales.''')
+
+opcion=int(input("Introduce una opción: "))
+
+while opcion<0 or opcion>5:
+    print ("Error, introduce una opción correcta")
+    opcion=int(input("Introduce una opción: "))
+
+while opcion!=0:
+    if opcion==1:
+        
+        for i in listar(datos):
+            print (i)
+        opcion=int(input("Introduce una opción: "))
+        while opcion<0 or opcion>5:
+            print ("Error, introduce una opción correcta")
+            opcion=int(input("Introduce una opción: "))
+    if opcion==2:
+        contador=len(contar(datos))
+        for i in contar(datos):
+            print (i)
+        print ("El número de programas de deportes es: " ,contador)
+        opcion=int(input("Introduce una opción: "))
+        while opcion<0 or opcion>5:
+            print ("Error, introduce una opción correcta")
+            opcion=int(input("Introduce una opción: "))
+    if opcion==3:
+        lista3=[]
+        canal=input("Introduce el canal que quieres buscar: ")
+        for i in buscar(datos,canal):
+            print(i)
+            lista3.append(i)
+        if len(lista3)==0:
+            print("El canal",canal," no existe o no tiene programación")
+        opcion=int(input("Introduce una opción: "))
+        while opcion<0 or opcion>5:
+            print ("Error, introduce una opción correcta")
+            opcion=int(input("Introduce una opción: "))
+    if opcion==4:
+        lista4=[]
+        programa=input("Introduce el nombre del proigrama que quieres buscar(Pon un espacio al final del nombre): ")
+        for i in informacion(datos,programa):
+            print(i)
+            lista4.append(i)
+        if len(lista4)==0:
+            print("El programa",programa," no existe o no está en la programación")
+        opcion=int(input("Introduce una opción: "))
+        while opcion<0 or opcion>5:
+            print ("Error, introduce una opción correcta")
+            opcion=int(input("Introduce una opción: "))
+    if opcion==5:
+        
+        opcion=int(input("Introduce una opción: "))
+        while opcion<0 or opcion>5:
+            print ("Error, introduce una opción correcta")
+            opcion=int(input("Introduce una opción: "))
+
+
+if opcion==0:
+    print("Fin del programa")
---
+++ Funciones.py
@@ -0,0 +1,17 @@
+from lxml import etree
+
+def listar (datos):
+    nombre=datos.xpath("/tv/channel/@id")
+    return nombre
+
+def contar (datos):
+    lista1=datos.xpath("/tv/programme[category='Sports']//title/text()")
+    return lista1
+
+def buscar (datos, canal):
+   busqueda=datos.xpath("/tv/programme[@channel='%s']//title/text()"%canal)
+   return busqueda
+
+def informacion (datos, programa):
+    info=datos.xpath("/tv/programme[title='%s']//@channel"%programa)
+    return info
